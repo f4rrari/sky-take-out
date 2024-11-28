d18c7063a74b9c97934fcd9916d5ac2a8b9d26c8
wen <2381835898@qq.com>
2024-11-28 09:55:27 +0800

    feat: 公共字段自动填充代码开发

--- sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -10,6 +10,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -20,6 +21,7 @@ import java.util.List;
 @RequestMapping("/admin/category")
 @Api(tags = "分类相关接口")
 @Slf4j
+@Service
 public class CategoryController {
 
     @Autowired
